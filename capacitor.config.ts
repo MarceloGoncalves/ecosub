67fea433a4dbe7e4da49d2b33ed1727c50393697
Marcelo Gonçalves <marcelogoncalvescdd@gmail.com>
2023-07-06 13:19:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'titan',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
